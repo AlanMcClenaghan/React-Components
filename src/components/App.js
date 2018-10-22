af6c649d792fd4c8304ccb450f1317298b8dc7e9
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2018-10-22 20:49:10 +0100

    Managing State and Data Flow

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,60 @@
 import React, { Component } from 'react';
-import Header from './Header'
-import Player from './Player'
+import Header from './Header';
+import Player from './Player';
+import AddPlayerForm from './AddPlayerForm';
 
 class App extends Component {
   state = {
     players: [
       {
         name: "Alan",
+        score: 0,
         id: 1
       },
       {
         name: "Ruth",
+        score: 0,
         id: 2
       },
       {
         name: "Matthew",
+        score: 0,
         id: 3
       },
       {
         name: "Lucy",
+        score: 0,
         id: 4
       }
     ]
   };
+
+  // player id counter
+  prevPlayerId = 4;
+
+  handleScoreChange = (index, delta) => {
+    this.setState(prevState => ({
+      score: prevState.players[index].score
+        += delta
+    }));
+    // console.log('index: ' + index, 'delta: ' + delta)
+  }
+
+  handleAddPlayer = (name) => {
+    // console.log(this.state.players);
+    this.setState(prevState => {
+      return {
+        players: [
+          ...prevState.players,
+          {
+            name,
+            score: 0,
+            id: this.prevPlayerId += 1,
+          }
+        ]
+      };
+    });
+  }
 
   handleRemovePlayer = (id) => {
     this.setState(prevState => {
@@ -37,18 +69,22 @@ class App extends Component {
       <div className="scoreboard">
         <Header
           title="Scoreboard"
-          totalPlayers={this.state.players.length}
+          players={this.state.players}
         />
 
         {/* Players list */}
-        {this.state.players.map(player =>
+        {this.state.players.map((player, index) =>
           <Player
             name={player.name}
+            score={player.score}
             id={player.id}
             key={player.id.toString()}
+            index={index}
+            changeScore={this.handleScoreChange}
             removePlayer={this.handleRemovePlayer}
           />
         )}
+        <AddPlayerForm addPlayer={this.handleAddPlayer} />
       </div>
     );
   }
